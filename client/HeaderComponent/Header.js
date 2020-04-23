0255efecf1b3429cc34045065540d98c9c9b5654
Arman Gurkan <arman@secelektrik.com>
2020-04-23 07:36:01 -0700

    completed the mobile header

--- client/HeaderComponent/Header.js
+++ client/HeaderComponent/Header.js
@@ -6,7 +6,9 @@ import React from 'react';
 const Header = () => {
 	return (
 			<div className="header">
-			
+				<div className="circle-frame">
+					<div className="circle"></div>
+				</div>
 			</div>
 	)
 }
